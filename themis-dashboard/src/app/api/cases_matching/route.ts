a7b9dd1e29e95b9f5ab67fefcf76d65980858ac6
Nancy Nabacwa <nancypaulinenabacwa@gmail.com>
2024-11-14 11:28:14 +0300

    Remove Comments

--- themis-dashboard/src/app/api/cases_matching/route.ts
+++ themis-dashboard/src/app/api/cases_matching/route.ts
@@ -1,28 +1,3 @@
-// import { NextResponse } from 'next/server';
-// export async function GET() {
-//     const transcriptionUrl = process.env.THEMIS_URL;
-
-//     try {
-//         const response = await fetch(`${transcriptionUrl}/api/case_laws/`, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
-
-//         if (!response.ok) {
-//             const errorData = await response.json();
-//             return NextResponse.json({ error: errorData.detail || 'Failed to fetch cases' }, { status: response.status });
-//         }
-
-//         const cases = await response.json();
-//         return NextResponse.json(cases, { status: 200 });
-//     } catch (error) {
-//         console.error('Error fetching cases:', error);
-//         return NextResponse.json({ error: 'An unexpected error occurred.' }, { status: 500 });
-//     }
-// }
-
 import { NextResponse } from 'next/server';
 export async function GET() {
     const transcriptionUrl = process.env.THEMIS_URL;
--- themis-dashboard/src/app/api/transcriptions/route.ts
+++ themis-dashboard/src/app/api/transcriptions/route.ts
@@ -33,44 +33,6 @@ export async function POST(request: NextRequest) {
 }
 
 
-
-// import { NextRequest, NextResponse } from 'next/server';
-// import { fetchWithTimeout } from '@/app/utils/fetchWithTimeout';
-
-// export async function POST(request: NextRequest) {
-//   const transcriptionUrl = process.env.THEMIS_URL;
-
-//   try {
-//     const { case_name, case_number, audio_file } = await request.json();
-
-//     if (!case_name || !case_number || !audio_file) {
-//       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
-//     }
-
-//     const response = await fetchWithTimeout(`${transcriptionUrl}/api/transcriptions/`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ case_name, case_number, audio_file }),
-//     }, 100000); // Set timeout for 60 seconds (or as long as needed)
-
-//     if (!response.ok) {
-//       const errorData = await response.json();
-//       return NextResponse.json({ error: errorData.detail || 'Failed to create transcription' }, { status: response.status });
-//     }
-
-//     const result = await response.json();
-//     return NextResponse.json(result, { status: 201 });
-//   } catch (error) {
-//     return NextResponse.json({ error: 'An unexpected error occurred.' }, { status: 500 });
-//   }
-// }
-
-
-
-
-
 export async function GET() {
     const transcriptionUrl = process.env.THEMIS_URL;
 
